7f53e3142d36248d24073ef4a725aba991c4db6d
Aude Lejuez <dusterherz@gmail.com>
2018-06-29 11:24:32 +0200

    Token authentification

--- auth.py
+++ auth.py
@@ -1,13 +1,13 @@
-from flask_httpauth import HTTPBasicAuth
+from flask_httpauth import HTTPTokenAuth
 from flask import g
 
 from models import User
 
-auth = HTTPBasicAuth()
+auth = HTTPTokenAuth()
 
 
-@auth.verify_password
-def verify_token(token, password):
+@auth.verify_token
+def verify_token(token):
     user = User.verify_auth_token(token)
     if not user:
         return False
